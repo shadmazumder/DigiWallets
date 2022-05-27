f29da8c18951818ad29093637de5c1570e67cd8c
shadmazumder <shadmazumder@gmail.com>
2022-05-27 18:39:21 +0600

    Moving RemoteLoader into production code

---
+++ CryptoLoader/CryptoLoader/RemoteLoader/RemoteLoader.swift
@@ -0,0 +1,40 @@
+//
+//  RemoteLoader.swift
+//  CryptoLoader
+//
+//  Created by Shad Mazumder on 27/5/22.
+//
+
+import Foundation
+
+public class RemoteLoader {
+    private let client: HTTPClient
+    
+    public init(client: HTTPClient) {
+        self.client = client
+    }
+    
+    public func load(from url: URL, completion: @escaping ((HTTPResult) -> Void)){
+        client.get(from: url) { [weak self] result in
+            self?.mapResult(result, into: completion)
+        }
+    }
+    
+    private func mapResult(_ result: HTTPResult, into completion: @escaping ((HTTPResult) -> Void)){
+        switch result {
+        case let .success(data, response):
+            completion(mapHTTPResult(with: data, for: response))
+        default:
+            completion(.failure(.connectivity))
+        }
+    }
+    
+    private func mapHTTPResult(with data: Data, for response: HTTPURLResponse) -> HTTPResult{
+        if response.statusCode == 200{
+            return .success(data, response)
+        }else{
+            return .failure(.non200HTTPResponse)
+        }
+    }
+    
+}
--- CryptoLoader/CryptoLoaderTests/RemoteLoaderTests.swift
+++ CryptoLoader/CryptoLoaderTests/RemoteLoaderTests.swift
@@ -8,38 +8,6 @@
 import XCTest
 import CryptoLoader
 
-class RemoteLoader {
-    private let client: HTTPClient
-    
-    init(client: HTTPClient) {
-        self.client = client
-    }
-    
-    func load(from url: URL, completion: @escaping ((HTTPResult) -> Void)){
-        client.get(from: url) { [weak self] result in
-            self?.mapResult(result, into: completion)
-        }
-    }
-    
-    private func mapResult(_ result: HTTPResult, into completion: @escaping ((HTTPResult) -> Void)){
-        switch result {
-        case let .success(data, response):
-            completion(mapHTTPResult(with: data, for: response))
-        default:
-            completion(.failure(.connectivity))
-        }
-    }
-    
-    private func mapHTTPResult(with data: Data, for response: HTTPURLResponse) -> HTTPResult{
-        if response.statusCode == 200{
-            return .success(data, response)
-        }else{
-            return .failure(.non200HTTPResponse)
-        }
-    }
-    
-}
-
 class RemoteLoaderTests: XCTestCase {
     func test_init_doesNotCallAPI() {
         let (_ , spy) = makeSUT()
