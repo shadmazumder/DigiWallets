caff194e1ddaa08e982b76dce1f4d20ef9ae5c68
shadmazumder <shadmazumder@gmail.com>
2022-05-27 19:34:25 +0600

    Adding URLProtocolStub for stubbing URLSession call

--- CryptoLoader/CryptoLoaderTests/URLSessionHTTPClientTests.swift
+++ CryptoLoader/CryptoLoaderTests/URLSessionHTTPClientTests.swift
@@ -9,3 +9,32 @@ import XCTest
 
 class URLSessionHTTPClientTests: XCTestCase {
 }
+
+
+class URLProtocolStub: URLProtocol {
+     private static var requestObservable: ((URLRequest) -> Void)?
+
+     static func observeRequest(observer: @escaping ((URLRequest)-> Void)){
+         requestObservable = observer
+     }
+
+     static func startInterceptingRequest(){
+         URLProtocol.registerClass(URLProtocolStub.self)
+     }
+
+     static func stopInterceptingRequest(){
+         URLProtocol.unregisterClass(URLProtocolStub.self)
+     }
+
+     override class func canInit(with request: URLRequest) -> Bool {
+         return true
+     }
+
+     override class func canonicalRequest(for request: URLRequest) -> URLRequest {
+         return request
+     }
+
+     override func startLoading() {
+         URLProtocolStub.requestObservable?(request)
+     }
+ }
