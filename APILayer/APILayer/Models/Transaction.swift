091d74c841ba962532540f56f5fe64c9e987a144
shadmazumder <shadmazumder@gmail.com>
2022-05-29 18:22:11 +0600

    Adding Transaction model

---
+++ APILayer/APILayer/Models/Transaction.swift
@@ -0,0 +1,17 @@
+//
+//  Transaction.swift
+//  APILayer
+//
+//  Created by Shad Mazumder on 29/5/22.
+//
+
+import Foundation
+
+struct Transaction {
+    let id: String
+    let entry: String
+    let amount: String
+    let currency: String
+    let sender: String
+    let recipient: String
+}
