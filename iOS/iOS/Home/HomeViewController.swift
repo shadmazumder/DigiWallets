e12e64e8e622c48aa68b60daf2eb4604a2aa9892
shadmazumder <shadmazumder@gmail.com>
2022-05-30 02:26:29 +0600

    Loading HomeViewController from storyboard

---
+++ iOS/iOS/Home/HomeViewController.swift
@@ -0,0 +1,10 @@
+//
+//  HomeViewController.swift
+//  iOS
+//
+//  Created by Shad Mazumder on 30/5/22.
+//
+
+import UIKit
+
+public class HomeViewController: UIViewController {}
--- iOS/iOSTests/HomeViewControllerTests.swift
+++ iOS/iOSTests/HomeViewControllerTests.swift
@@ -6,5 +6,17 @@
 //
 
 import XCTest
+import iOS
 
-class HomeViewControllerTests: XCTestCase {}
+class HomeViewControllerTests: XCTestCase {
+    func test_loadFromStoryboard_returnsHomeViewController() {
+        XCTAssertTrue(launchesViewControllerFromHomeSotyboard() is HomeViewController, "Initial ViewController is not HomeViewController")
+    }
+}
+
+// MARK: - Helper
+private func launchesViewControllerFromHomeSotyboard() -> UIViewController? {
+    let bundle = Bundle(for: HomeViewController.self)
+    let storyboard = UIStoryboard(name: "Home", bundle: bundle)
+    return storyboard.instantiateInitialViewController()
+}
