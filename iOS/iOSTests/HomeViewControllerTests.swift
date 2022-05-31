1991dfb76f1e2182fe653683be087e7e1fbca5c1
shadmazumder <shadmazumder@gmail.com>
2022-05-31 16:32:06 +0600

    Adding ClientSpy into HomeViewControllerTests file

--- iOS/iOSTests/HomeViewControllerTests.swift
+++ iOS/iOSTests/HomeViewControllerTests.swift
@@ -7,6 +7,7 @@
 
 import XCTest
 import iOS
+import CryptoLoader
 
 class HomeViewControllerTests: XCTestCase {
     func test_loadFromStoryboard_returnsHomeViewController() {
@@ -28,5 +29,24 @@ class HomeViewControllerTests: XCTestCase {
         let bundle = Bundle(for: HomeViewController.self)
         let storyboard = UIStoryboard(name: "Home", bundle: bundle)
         return storyboard.instantiateInitialViewController()
+    }
+}
+
+class ClientSpy: HTTPClient {
+    typealias Result = (url: URL, completion: ((HTTPResult) -> Void))
+    var message = [Result]()
+    
+    init() {}
+    
+    func get(from url: URL, completion: @escaping ((HTTPResult) -> Void)) {
+        message.append((url, completion))
+    }
+    
+    func completeWith(_ data: Data, response: HTTPURLResponse, index: Int = 0) {
+        message[index].completion(.success(data, response))
+    }
+    
+    func completeWithError(_ error: RemoteLoader.ResultError, index: Int = 0) {
+        message[index].completion(.failure(error))
     }
 }
