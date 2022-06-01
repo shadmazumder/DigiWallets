6d830e86def20d6b2db0a1eff50b6bc375fb6ddd
shadmazumder <shadmazumder@gmail.com>
2022-06-01 12:06:56 +0600

    Providing urls as SUT method params

--- iOS/iOSTests/HomeViewControllerTests.swift
+++ iOS/iOSTests/HomeViewControllerTests.swift
@@ -24,7 +24,7 @@ class HomeViewControllerTests: XCTestCase {
     }
     
     func test_loadView_returnsErrorOnUnsetURLs() {
-        let (sut, delegate, _) = makeSUt()
+        let (sut, delegate, _) = makeSUt(nil, nil)
         
         sut.loadViewIfNeeded()
 
@@ -32,20 +32,18 @@ class HomeViewControllerTests: XCTestCase {
     }
     
     func test_loadView_loadURLsWithoutError() {
-        let (sut, delegate, client) = makeSUt()
         let walletsURL = URL(string: "any-wallets-url")!
-        let transactionURL = URL(string: "any-transactions-url")!
-        sut.walletsURL = walletsURL
-        sut.transactionsURL = transactionURL
+        let transactionsURL = URL(string: "any-transactions-url")!
+        let (sut, delegate, client) = makeSUt(walletsURL, transactionsURL)
         
         sut.loadViewIfNeeded()
 
         XCTAssertNil(delegate.errorResult)
-        XCTAssertEqual(client.message.map({$0.url}), [walletsURL, transactionURL])
+        XCTAssertEqual(client.message.map({$0.url}), [walletsURL, transactionsURL])
     }
     
     // MARK: - Helper
-    private func makeSUt() -> (sut: HomeViewController, delegate: HomeViewControllerDelegateSpy, client: ClientSpy){
+    private func makeSUt(_ walletsURL: URL? = URL(string: "any-wallets-url")!, _ transactionsURL: URL? = URL(string: "any-transactions-url")!) -> (sut: HomeViewController, delegate: HomeViewControllerDelegateSpy, client: ClientSpy){
         let client = ClientSpy()
         let loader = DecodableRemoteLoader(client)
         let delegate = HomeViewControllerDelegateSpy()
@@ -53,6 +51,9 @@ class HomeViewControllerTests: XCTestCase {
         
         homeViewController.delegate = delegate
         homeViewController.loader = loader
+        
+        homeViewController.walletsURL = walletsURL
+        homeViewController.transactionsURL = transactionsURL
         
         return (homeViewController, delegate, client)
     }
