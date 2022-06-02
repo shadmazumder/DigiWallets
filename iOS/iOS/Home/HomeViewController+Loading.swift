4c29e254749bad0b10e4998ae1e89f0626b52bbe
shadmazumder <shadmazumder@gmail.com>
2022-06-02 23:26:38 +0600

    Extracting remote loading into a separated file

---
+++ iOS/iOS/Home/HomeViewController+Loading.swift
@@ -0,0 +1,59 @@
+//
+//  HomeViewController+Loading.swift
+//  iOS
+//
+//  Created by Shad Mazumder on 2/6/22.
+//
+
+import UIKit
+import APILayer
+
+extension HomeViewController{
+    @objc func loadFromRemote(){
+        guard let walletsURL = walletsURL,  let transactionsURL = transactionsURL else{
+            delegate?.handleErrorState(HomeViewControllerError.unsetURLs)
+            return
+        }
+
+        refreshControl?.beginRefreshing()
+        loadWallets(from: walletsURL)
+        loadTransactions(from: transactionsURL)
+    }
+    
+    private func loadWallets(from url: URL){
+        loader?.load(from: url, of: Wallets.self, completion: { [weak self] result in
+            self?.refreshControl?.endRefreshing()
+            
+            switch result {
+            case let .success(walletsAPIModel):
+                let wallets = walletsAPIModel as! Wallets
+                self?.diffarableReload(wallets.walletsViewModel, to: .wallets)
+            case let .failure(error):
+                self?.delegate?.handleErrorState(error)
+            }
+        })
+    }
+    
+    private func diffarableReload(_ homeViewModel: [AnyHashable], to section: HomeViewSection){
+        DispatchQueue.main.async { [weak self] in
+            guard var snapShot = self?.dataSource.snapshot() else {return}
+            
+            snapShot.appendItems(homeViewModel, toSection: section)
+            self?.dataSource.applySnapshotUsingReloadData(snapShot)
+        }
+    }
+    
+    private func loadTransactions(from url: URL){
+        loader?.load(from: url, of: Histories.self, completion: { [weak self] result in
+            self?.refreshControl?.endRefreshing()
+            
+            switch result {
+            case let .success(historiesAPIModel):
+                let histories = historiesAPIModel as! Histories
+                self?.diffarableReload(histories.transactionsViewModel, to: .transaction)
+            case let .failure(error):
+                self?.delegate?.handleErrorState(error)
+            }
+        })
+    }
+}
