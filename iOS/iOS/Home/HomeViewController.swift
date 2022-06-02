e2a63fc6b4dd6cda5b5fca52701e38029638c2a5
shadmazumder <shadmazumder@gmail.com>
2022-06-02 23:23:22 +0600

    Updating TableView
    Updating section and item type.
    Firing UI update from main thread

--- iOS/iOS/Home/HomeViewController.swift
+++ iOS/iOS/Home/HomeViewController.swift
@@ -23,7 +23,7 @@ public class HomeViewController: UITableViewController {
     public var walletsURL: URL?
     public var transactionsURL: URL?
     
-    public private(set) lazy var dataSource: UITableViewDiffableDataSource<Int, HomeViewModel> = {
+    public private(set) lazy var dataSource: UITableViewDiffableDataSource<HomeViewSection, AnyHashable> = {
         .init(tableView: tableView) { (_, _, _) in
             UITableViewCell()
         }
@@ -33,12 +33,18 @@ public class HomeViewController: UITableViewController {
         super.viewDidLoad()
         
         addRefreshControll()
+        addSections()
         loadFromRemote()
     }
     
     private func addRefreshControll(){
         refreshControl = UIRefreshControl()
         refreshControl?.addTarget(self, action: #selector(loadFromRemote), for: .valueChanged)
+    }
+    
+    private func addSections(){
+        var snapShot = NSDiffableDataSourceSnapshot<HomeViewSection, AnyHashable>()
+        snapShot.appendSections(HomeViewSection.allCases)
     }
     
     @objc private func loadFromRemote(){
@@ -54,21 +60,33 @@ public class HomeViewController: UITableViewController {
     private func loadWallets(from url: URL){
         loader?.load(from: url, of: Wallets.self, completion: { [weak self] result in
             self?.refreshControl?.endRefreshing()
+            
             switch result {
-            case .success(_):
-                break
+            case let .success(walletsAPIModel):
+                let wallets = walletsAPIModel as! Wallets
+                self?.diffarableReload(wallets.walletsViewModel, to: .wallets)
             case let .failure(error):
                 self?.delegate?.handleErrorState(error)
             }
         })
     }
     
+    private func diffarableReload(_ homeViewModel: [AnyHashable], to section: HomeViewSection){
+        DispatchQueue.main.async { [weak self] in
+            guard var snapShot = self?.dataSource.snapshot() else {return}
+            
+            snapShot.appendItems(homeViewModel, toSection: section)
+            self?.dataSource.applySnapshotUsingReloadData(snapShot)
+        }
+    }
+    
     private func loadTransactions(from url: URL){
         loader?.load(from: url, of: Histories.self, completion: { [weak self] result in
             self?.refreshControl?.endRefreshing()
             switch result {
-            case .success(_):
-                break
+            case let .success(historiesAPIModel):
+                let histories = historiesAPIModel as! Histories
+                self?.diffarableReload(histories.transactionsViewModel, to: .transaction)
             case let .failure(error):
                 self?.delegate?.handleErrorState(error)
             }
--- iOS/iOS/Home/HomeViewModel.swift
+++ iOS/iOS/Home/HomeViewModel.swift
@@ -6,5 +6,20 @@
 //
 
 import Foundation
+import APILayer
 
-public struct HomeViewModel: Hashable{}
+public enum HomeViewSection: String, CaseIterable{
+    case wallets = "My Wallets"
+    case transaction = "Transactions"
+}
+
+public struct WalletViewModel: Hashable{}
+extension Wallets{
+    var walletsViewModel: [WalletViewModel]{[]}
+}
+
+public struct TransactionViewModel: Hashable{}
+extension Histories{
+    var transactionsViewModel: [TransactionViewModel]{[]}
+}
+
