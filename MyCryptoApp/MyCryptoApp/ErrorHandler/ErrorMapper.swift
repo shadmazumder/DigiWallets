723a350ea919b87c52866f29006f3a2c1dc87b4e
shadmazumder <shadmazumder@gmail.com>
2022-06-05 17:26:19 +0600

    Implementing Error handling

---
+++ MyCryptoApp/MyCryptoApp/ErrorHandler/ErrorMapper.swift
@@ -0,0 +1,30 @@
+//
+//  ErrorMapper.swift
+//  MyCryptoApp
+//
+//  Created by Shad Mazumder on 5/6/22.
+//
+
+import Foundation
+import CryptoLoader
+
+typealias AlertContent = (title: String, message: String, action: String)
+
+struct ErrorMapper {
+    func alertContent(for error: Error) -> AlertContent?{
+        switch error {
+        case RemoteLoader.ResultError.connectivity:
+            return connectivityAlertContent()
+        default:
+            RemoteTracker.track(error)
+            return nil
+        }
+    }
+    
+    private func connectivityAlertContent() -> AlertContent{
+        let title = "Oop!"
+        let message = "😬 We lost the connection 🙈"
+        let action = "Try again later"
+        return (title, message, action)
+    }
+}
---
+++ MyCryptoApp/MyCryptoApp/ErrorHandler/RemoteTracker.swift
@@ -0,0 +1,14 @@
+//
+//  RemoteTracker.swift
+//  MyCryptoApp
+//
+//  Created by Shad Mazumder on 5/6/22.
+//
+
+import Foundation
+
+struct RemoteTracker {
+    static func track(_ error: Error){
+        // will track the error on some remote server/service
+    }
+}
