94daf14b1374c9862495b36f28b67a13136402f1
shadmazumder <shadmazumder@gmail.com>
2022-06-05 12:50:23 +0600

    Initiating iOS app

---
+++ MyCryptoApp/MyCryptoApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyCryptoApp
+//
+//  Created by Shad Mazumder on 5/6/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
