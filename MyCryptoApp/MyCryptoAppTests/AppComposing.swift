97895af149e3516108c81fb554e7f2f6c2c4edf2
shadmazumder <shadmazumder@gmail.com>
2022-06-05 13:27:20 +0600

    Starting the Router returns a UINavigationController

--- MyCryptoApp/MyCryptoAppTests/AppComposing.swift
+++ MyCryptoApp/MyCryptoAppTests/AppComposing.swift
@@ -7,5 +7,17 @@
 
 import XCTest
 
+struct Router {
+    func startRouting() -> UIViewController{
+        UINavigationController()
+    }
+}
+
 class AppComposing: XCTestCase {
+    func test_startRouting_returnsNavigationController() {
+        let router = Router()
+        let controller = router.startRouting()
+        
+        XCTAssertTrue(controller is UINavigationController)
+    }
 }
