61f04b5713509542504de358d7837d7d571d28ef
shadmazumder <shadmazumder@gmail.com>
2022-06-05 12:31:14 +0600

    Extracting TransactionViewController class creation into separated concerns

--- iOS/iOSTests/TransactionDetailTests.swift
+++ iOS/iOSTests/TransactionDetailTests.swift
@@ -11,13 +11,28 @@ import iOS
 class TransactionDetailTests: XCTestCase {
     func test_init_rendersNothing() {
         let sut = makeSUT()
-        sut.loadViewIfNeeded()
+        
         XCTAssertEqual(sut.transactionDetails.text?.isEmpty, true)
         XCTAssertEqual(sut.senderName.text?.isEmpty, true)
         XCTAssertEqual(sut.amount.text?.isEmpty, true)
     }
     
     private func makeSUT() -> TransactionViewController{
+        let sut = TransactionUIComposer.composeTransaction()
+        sut.loadViewIfNeeded()
+        return sut
+    }
+}
+
+public class TransactionUIComposer{
+    private init(){}
+    public static func composeTransaction() -> TransactionViewController{
+        TransactionViewController.make()
+    }
+}
+
+private extension TransactionViewController{
+    static func make() -> TransactionViewController{
         let bundle = Bundle(for: HomeViewController.self)
         let storyboard = UIStoryboard(name: "Transaction", bundle: bundle)
         let transactionDetailViewController = storyboard.instantiateInitialViewController() as! TransactionViewController
