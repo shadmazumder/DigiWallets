f1ae7dfcdd079c5aba9358ff2aa47ab1888b605e
shadmazumder <shadmazumder@gmail.com>
2022-06-05 16:27:21 +0600

    Holding transaction state to update the UI once it is fully loaded

--- iOS/iOS/TransactionDetails/TransactionViewController.swift
+++ iOS/iOS/TransactionDetails/TransactionViewController.swift
@@ -12,9 +12,15 @@ public class TransactionViewController: UIViewController {
     @IBOutlet public private(set) weak var senderName: UILabel!
     @IBOutlet public private(set) weak var amount: UILabel!
     
+    private var transaction: Transaction?
+    
+    public override func viewDidLoad() {
+        transactionDetails.text = transaction?.description
+        senderName.text = transaction?.sender
+        amount.text = transaction?.amount
+    }
+    
     public func update(with transaction: Transaction){
-        transactionDetails.text = transaction.description
-        senderName.text = transaction.sender
-        amount.text = transaction.amount
+        self.transaction = transaction
     }
 }
--- iOS/iOSTests/TransactionDetailTests.swift
+++ iOS/iOSTests/TransactionDetailTests.swift
@@ -10,28 +10,25 @@ import iOS
 
 class TransactionDetailTests: XCTestCase {
     func test_init_rendersNoDetails() {
-        let sut = makeSUT()
+        let sut = TransactionUIComposer.composeTransaction()
         
-        XCTAssertEqual(sut.transactionDetails.text?.isEmpty, true)
-        XCTAssertEqual(sut.senderName.text?.isEmpty, true)
-        XCTAssertEqual(sut.amount.text?.isEmpty, true)
+        sut.loadViewIfNeeded()
+        
+        XCTAssertNil(sut.transactionDetails.text)
+        XCTAssertNil(sut.senderName.text)
+        XCTAssertNil(sut.amount.text)
     }
     
     func test_update_rendersDetails(){
         let transaction = Transaction(id: "any id", description: "any description", sender: "any sender", amount: "Any amount")
-        
-        let sut = makeSUT()
+        let sut = TransactionUIComposer.composeTransaction()
         sut.update(with: transaction)
+
+        sut.loadViewIfNeeded()
+        
         
         XCTAssertEqual(sut.transactionDetails.text, transaction.description)
         XCTAssertEqual(sut.senderName.text, transaction.sender)
         XCTAssertEqual(sut.amount.text, transaction.amount)
-    }
-    
-    // MARK: - Helper
-    private func makeSUT() -> TransactionViewController{
-        let sut = TransactionUIComposer.composeTransaction()
-        sut.loadViewIfNeeded()
-        return sut
     }
 }
